dec42c2bf759a42ff21404a9920c3f5d89e1b104
dsong2548 <dsong2548@hwdsb.on.ca>
2018-01-12 10:32:09 -0500

    * .gitignore: Initial commit
    * Bitcoin.sln:
    * Main.cs:
    * Info.plist:
    * App.xaml:
    * AppDelegate.cs:
    * packages.config:
    * App.xaml.cs:
    * MainActivity.cs:
    * packages.config:
    * Entitlements.plist:
    * Bitcoin.iOS.csproj:
    * Bitcoin.csproj:
    * packages.config:
    * BitcoinPage.xaml:
    * Bitcoin.Droid.csproj:
    * LaunchScreen.storyboard:
    * BitcoinPage.xaml.cs:
    * AboutAssets.txt:
    * AssemblyInfo.cs:
    * Contents.json:
    * styles.xml:
    * icon.png:
    * Tabbar.axml:
    * AboutResources.txt:
    * AssemblyInfo.cs:
    * Toolbar.axml:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * Contents.json:

---
+++ Bitcoin/BitcoinPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Xamarin.Forms;
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Bitcoin
+{
+    public partial class BitcoinPage : ContentPage
+    {
+        static Dictionary<String, String> currencySymbols = new Dictionary<String, String>
+        {
+            ["USD"] = "$",
+            ["GBP"] = "£",
+            ["EUR"] = "€"
+        };
+        string selectedCurrency = "USD";
+
+        public BitcoinPage()
+        {
+            InitializeComponent();
+
+            UpdateBitcoinLabel();
+        }
+
+        async Task UpdateBitcoinLabel(bool startTimer = true)
+        {
+            const string bitcoinAddress = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            var client = new HttpClient();
+            var bitcoinJSON = await client.GetStringAsync(bitcoinAddress);
+            var bitcoinObject = JObject.Parse(bitcoinJSON);
+            var bitcoinPrice = (double)bitcoinObject["bpi"][selectedCurrency]["rate_float"];
+            BitcoinPriceLabel.Text = String.Format("{0}{1:.00}", currencySymbols[selectedCurrency], bitcoinPrice);
+            if (startTimer)
+                Device.StartTimer(TimeSpan.FromMinutes(1), () =>
+                {
+                    UpdateBitcoinLabel();
+                    return false;
+                });
+        }
+
+        void ChangeCurrency(object sender, EventArgs e)
+        {
+            selectedCurrency = ((Button)sender).Text;
+            foreach (var child in BitcoinGrid.Children)
+            {
+                if (child.GetType().ToString() == "Xamarin.Forms.Button")
+                {
+                    if (selectedCurrency == ((Button)child).Text)
+                    {
+                        ((Button)child).BackgroundColor = Color.White;
+                        ((Button)child).TextColor = Color.FromHex("#202020");
+                    }
+                    else
+                    {
+                        ((Button)child).BackgroundColor = Color.FromHex("#202020");
+                        ((Button)child).TextColor = Color.White;
+                    }
+                }
+            }
+            UpdateBitcoinLabel(false);
+        }
+    }
+}
